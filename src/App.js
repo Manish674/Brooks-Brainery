1a1453131daf8dba85a17f7d4c46ec100793c1bf
Manish674 <manishchoudhary4905@gmail.com>
2021-05-25 19:26:08 +0530

    Starting summer program page

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Home";
+import Aboutpage from "./pages/About";
+import ContactUs from "./pages/Contact";
+import Footer from "./components/Footer";
+import { Route, Switch } from "react-router";
+import JoinForm from "./components/JForm";
+import SummerProgram from "./pages/SummerProgram";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => {
+            return (
+              <>
+                <Homepage />
+                <Aboutpage />
+                <ContactUs />
+              </>
+            );
+          }}
+        />
+        <Route path="/join-bb" component={JoinForm} />
+        <Route path="/summer-program" component={SummerProgram} />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/SummerProgram.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SummerProgram = () => {
+  return (
+    <div className="">
+      <h1 className="">Summer Programmes</h1>
+      <span className=""></span>
+    </div>
+  );
+};
+
+export default SummerProgram;
