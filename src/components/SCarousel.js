7d718184219480783f6b6b1c744ffa0fa542566c
Manish674 <manishchoudhary4905@gmail.com>
2021-05-28 08:07:01 +0530

    Working on summer program carousels

--- src/components/SCarousel.js
+++ src/components/SCarousel.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { BiRightArrowAlt } from "react-icons/bi";
 
-const SCarousel = () => {
+const SCarousel = ({ svg, title }) => {
   return (
-    <>
-      <h3>Hello world</h3>
-    </>
+    <div className="max-w-[35rem] flex flex-col space-y-[1rem] items-center bg-[yellow] text-black w-4/5 ml-3">
+      <div className="mt-[1rem] flex justify-center">{svg}</div>
+      <h2 className="text-[1.2rem] font-semibold px-8">{title}</h2>
+      <div className="flex justify-end w-4/5 mx-auto">
+        <BiRightArrowAlt className="text-3xl mb-[1rem]" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/SummerProgram.js
+++ src/pages/SummerProgram.js
@@ -1,10 +1,66 @@
 import React from "react";
+import SCarousel from "../components/SCarousel";
+import Telescope from "../imgs/Telescope";
+import { Carousel } from "react-responsive-carousel";
+import {
+  plus,
+  biomedical,
+  gene,
+  calculus,
+  selfDriving,
+} from "../imgs/SummerProgramSvgs";
+
+const sciMath = [
+  {
+    svg: plus,
+    title: "Medical Startups",
+  },
+  {
+    svg: biomedical,
+    title: "Biomedical Engineering",
+  },
+  {
+    svg: gene,
+    title: "Gene editing",
+  },
+  {
+    svg: calculus,
+    title: "Calculus",
+  },
+  {
+    svg: selfDriving,
+    title: "Self Driving Vehicles ",
+  },
+];
 
 const SummerProgram = () => {
   return (
     <div className="">
-      <h1 className="">Summer Programmes</h1>
-      <span className=""></span>
+      <h1
+        style={{ fontSize: "clamp(1.75rem, 5vw, 46px)" }}
+        className="font-bold w-[45%]"
+      >
+        Summer Programmes
+      </h1>
+      <div className="border mt-[2.5rem]">
+        <div className="flex items-center justify-between">
+          <span
+            style={{ fontSize: "clamp(1.4rem, 5vw, 35px)" }}
+            className="font-semibold"
+          >
+            <h2>Science &</h2>
+            <h2>Maths</h2>
+          </span>
+          <div className="w-[40%]">{Telescope}</div>
+        </div>
+        <div className="mt-[2rem]">
+          <Carousel emulateTouch="true">
+            {sciMath.map(({ svg, title }) => (
+              <SCarousel svg={svg} title={title} />
+            ))}
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 };
